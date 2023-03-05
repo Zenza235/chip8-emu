8fe23f47ff2f0c5a72b650f9c93174a8431ba55b
Zenza235 <paoloatienza235@gmail.com>
2023-03-05 08:01:50 -0500

    updated ESC check in keyCallback

--- src/main.cpp
+++ src/main.cpp
@@ -88,7 +88,7 @@ static void keyCallback(GLFWwindow* window, int key, int scancode, int action, i
     if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
         glfwSetWindowShouldClose(window, GLFW_TRUE);
     
-    if (input_map.find(key) != input_map.end()) {
+    else if (input_map.find(key) != input_map.end()) {
         int index = input_map.at(key);
         if (action == GLFW_PRESS)
             chip8.key[index] = 1;
